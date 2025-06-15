d1e2b2697e92b8a25c9cdec8594bbfa06cabf995
Dhruv <dbpthk@gmail.com>
2025-06-15 19:41:01 +1000

    Collaboration section complete

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -20,6 +20,7 @@ const Section = ({
       ${className || ""}`}
     >
       {children}
+
       <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10" />
       <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10" />
 
@@ -27,10 +28,10 @@ const Section = ({
         <>
           <div
             className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 ${
-              crossOffset && crossesOffset
+              crossesOffset && crossesOffset
             } pointer-events-none lg:block xl:left-10 right-10`}
           />
-          <Section crossesOffset={crossesOffset} />
+          <SectionSvg crossesOffset={crossesOffset} />
         </>
       )}
     </div>
