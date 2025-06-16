3694c781d2cf48a68ed55397283ede90eed20f88
Dhruv <dbpthk@gmail.com>
2025-06-16 20:28:28 +1000

    Roadmap section complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -17,6 +19,7 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
       </div>
       <ButtonGradient />
     </>
